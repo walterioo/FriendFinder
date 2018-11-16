5bba79daac869e2bb70af4f45cf1323650ac053c
Walter A <39980099+walterioo@users.noreply.github.com>
2018-11-15 19:12:31 -0700

    html done

--- app/public/assets/js/main.js
+++ app/public/assets/js/main.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+console.log("js loaded");
+
+$('#submit').on('click', function (event)  {
+    console.log("click");
+    
+    event.preventDefault();
+    var newFriend = {
+        name: $("#name").val(),
+        photo: $("#photo").val(),
+        scores: [
+            $("#q1").val(),
+            $("#q2").val(),
+            $("#q3").val(),
+            $("#q4").val(),
+            $("#q5").val(),
+            $("#q6").val(),
+            $("#q7").val(),
+            $("#q8").val(),
+            $("#q9").val(),
+            $("#q10").val()
+        ],
+    };
+   $.post("/api/friends/",newFriend, (data) => {
+       console.log("post!");
+       
+       $("#friend-match").text(data.name);
+       $("image-match").attr("src", data.photo);
+       $("#modal").modal("toggle");
+   });
+
+});
