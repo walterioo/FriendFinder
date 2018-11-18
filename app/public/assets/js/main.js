687670a983c4d192b11531e3adfb01840f0bdff4
Walter A <39980099+walterioo@users.noreply.github.com>
2018-11-17 17:12:28 -0700

    App finished, validation and modal working

--- app/public/assets/js/main.js
+++ app/public/assets/js/main.js
@@ -1,32 +1,57 @@
-console.log("js loaded");
 $(document).ready(function () {
+    $('#modal').on('hidden.bs.modal', function () {
+        location.assign("/");
+    })
+    
     $('#submit').on('click', function (event) {
-        console.log("click");
-
         event.preventDefault();
-        var newFriend = {
-            name: $("#name").val(),
-            photo: $("#photo").val(),
-            scores: [
-                $("#q1").val(),
-                $("#q2").val(),
-                $("#q3").val(),
-                $("#q4").val(),
-                $("#q5").val(),
-                $("#q6").val(),
-                $("#q7").val(),
-                $("#q8").val(),
-                $("#q9").val(),
-                $("#q10").val()
-            ],
-        };
-        $.post("/api/friends/", newFriend, (data) => {
-            console.log("post!");
+        if (validateForm()) {
+            var newFriend = {
+                name: $("#name").val(),
+                photo: $("#photo").val(),
+                scores: [
+                    $("#q1").val(),
+                    $("#q2").val(),
+                    $("#q3").val(),
+                    $("#q4").val(),
+                    $("#q5").val(),
+                    $("#q6").val(),
+                    $("#q7").val(),
+                    $("#q8").val(),
+                    $("#q9").val(),
+                    $("#q10").val()
+                ],
+            };
 
-            $("#friend-match").text(data.name);
-            $("image-match").attr("src", data.photo);
-            $("#modal").modal("toggle");
-        });
-
+            $.post("/api/friends/", newFriend, (data) => {
+                $("#friend-match").text(data.name);
+                $("#photo-match").attr("src", data.photo);
+                $("#modal").modal("show");
+            });
+        } else {  
+            alert("Please complete de survey");
+        }
     });
 });
+
+function validateForm() {   
+    let valid = true;         
+    $('.form-control').each(function () {
+        if ($(this).val() === "") {
+            valid =  false;
+        }
+    });
+
+    $('.select').each(function () {
+        if ($(this).val() === "") {
+            valid =  false;
+        }
+    });
+
+    if (!$("#photo").val().match(/.(jpg|jpeg|png|gif)$/i)){
+        alert("Image file not valid");
+        valid = false;
+    }
+
+    return valid; 
+};
