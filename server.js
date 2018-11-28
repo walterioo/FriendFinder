f0f1c092bb3b6d0759f6806f5a34ebf734c76309
Walter A <39980099+walterioo@users.noreply.github.com>
2018-11-27 17:15:25 -0700

    cleaned code

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // DEPENDENCIES
 const express = require('express'),
- path = require('path'),
- htmlRoute = require('./app/routes/htmlRoutes'),
- apiRoute = require('./app/routes/apiRoutes');
+  path = require('path'),
+  htmlRoute = require('./app/routes/htmlRoutes'),
+  apiRoute = require('./app/routes/apiRoutes');
 
 // Express Config
 const app = express(),
-    PORT = process.env.PORT || 3000;
-app.use(express.urlencoded({ extended: true }));
+  PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 //Static File Serving
@@ -19,6 +21,5 @@ app.use("/", htmlRoute);
 
 // Server Listener
 app.listen(PORT, () => {
-    console.log(`Server listening on PORT: ${PORT}`);
+  console.log(`Server listening on PORT: ${PORT}`);
 });
-
